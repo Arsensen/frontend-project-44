4968d0ab86938dbe99c697c2caa4be2b677d4730
Arthur S <artysirazi@gmail.com>
2026-03-13 16:45:20 +0300

    feat(games): add prime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+import { baseQuestion } from "../src/basic-question.js";
+import { startGame } from "../src/index.js";
+
+const isPrime = (num) => {
+    if (num <= 1) {
+        return false;
+    }
+
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+const generatePrimeQuestion = () => {
+    const number = Math.floor(Math.random() * 100);
+    const correctAnswer = isPrime(number) ? 'yes' : 'no';
+
+    return { question: String(number), correctAnswer };
+};
+
+export const startPrimeGame = (name) => {
+    for (let i = 0; i < 3; i++) {
+        const { question, correctAnswer } = generatePrimeQuestion();
+        const answer = baseQuestion(question);
+        if (answer === correctAnswer) {
+            console.log('Correct!');
+        } else {
+            throw new Error(`${answer} is wrong answer ;(. Correct answer was '${correctAnswer}'. Let's try again, ${name}!`);
+        }
+    }
+};
+
+startGame(startPrimeGame, 'Answer "yes" if given number is prime. Otherwise answer "no".');
